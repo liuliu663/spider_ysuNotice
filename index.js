7cbff2c1ae2c952619c0b700813d4250f1476b43
puresox <13004384112@163.com>
2017-04-29 15:30:07 +0800

    bug

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 const moment = require('moment');
 
+const errRseult = [];
+
 const spiderNotice = ($, i) => {
   const length = $('li', '#main_mid_content').length;
   if (i < length) {
     const title = $('a', '#main_mid_content').eq(i).attr('title')
-            .trim()
-            .replace(/\//g, '-')
-            .replace(/</g, '《')
-            .replace(/>/g, '》');
+      .trim()
+      .replace(/\//g, '-')
+      .replace(/</g, '《')
+      .replace(/>/g, '》');
     let href = $('a', '#main_mid_content').eq(i).attr('href').trim();
     const patt = new RegExp('http://');
     if (!patt.test(href)) {
@@ -32,33 +34,45 @@ const spiderNotice = ($, i) => {
     try {
       fs.mkdirSync(`result/${year}/${month}/${day}`);
     } catch (e) {}
+
     return new Promise((resolve, reject) => {
-      setTimeout(resolve, 500);
+      setTimeout(resolve, 1);
     })
-            .then(() => new Promise((resolve, reject) => {
-              request(href, {
-                timeout: 3000,
-              }, (error, response, body) => {
-                if (error) {
-                  reject(error.code);
-                } else {
-                  resolve(cheerio.load(body));
-                }
-              });
-            }))
-            .then(data => fs.writeFileSync(`result/${year}/${month}/${day}/${title}.html`, data))
-            .then(() => {
-              console.log(`${i + 1}/${length}`);
-              return spiderNotice($, i + 1);
-            })
-            .catch(error =>
-              // console.log(error);
-               spiderNotice($, i));
+      .then(() => new Promise((resolve, reject) => {
+        request(href, {
+          timeout: 3000,
+        }, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(cheerio.load(body));
+          }
+        });
+      }))
+      .then((data) => {
+        fs.writeFileSync(`result/${year}/${month}/${day}/${title}.html`, data);
+        // console.log(`${i + 1}/${length}`);
+        return spiderNotice($, i + 1);
+      })
+      .catch((error) => {
+        if (error.code !== 'ETIMEDOUT') {
+          console.log(error);
+        }
+        const patto = new RegExp('info');
+        if (!patto.test(href)) {
+          fs.writeFileSync(`result/${year}/${month}/${day}/${title}.html`, '');
+          // console.log(`${i + 1}/${length}`);
+          errRseult.push(`result/${year}/${month}/${day}/${title}.html`);
+          return spiderNotice($, i + 1);
+        }
+        return spiderNotice($, i);
+      });
   }
 };
 
 const spiderList = (p) => {
   if (p === 359) {
+    fs.writeFileSync('errRseult.txt', errRseult);
     console.log('完成！');
   } else {
     new Promise((resolve, reject) => {
@@ -66,21 +80,23 @@ const spiderList = (p) => {
         timeout: 3000,
       }, (error, response, body) => {
         if (error) {
-          reject(error.code);
+          reject(error);
         } else {
           resolve(cheerio.load(body));
         }
       });
     })
-            .then($ => spiderNotice($, 0))
-            .then(() => {
-              console.log(`${p}/358`);
-              spiderList(p + 1);
-            })
-            .catch((error) => {
-              // console.log(error);
-              spiderList(p);
-            });
+      .then($ => spiderNotice($, 0))
+      .then(() => {
+        console.log(`${p}/358`);
+        spiderList(p + 1);
+      })
+      .catch((error) => {
+        if (error.code !== 'ETIMEDOUT') {
+          console.log(error);
+        }
+        spiderList(p);
+      });
   }
 };
 spiderList(1);
